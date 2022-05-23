78400a0555a5f9d6c4514b6ead80799f41848a59
Kov <44253207+kovzhu@users.noreply.github.com>
2022-05-23 15:21:10 +0800

    Update IOC recruits.py

--- IOC recruits.py
+++ IOC recruits.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+# from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -10,226 +11,164 @@ from bs4 import BeautifulSoup as bs
 import requests
 import random
 
-
 # url_bp = r'https://www.linkedin.com/jobs/search/?f_C=1389%2C1391&geoId=92000000'
 url_bp=r'https://www.linkedin.com/company/bp/jobs/'
 
-wd= webdriver.Chrome(executable_path='C:\chromedriver.exe')
 
-# Login
-wd.get(url_bp)
-time.sleep(5)
-wd.find_element(By.CSS_SELECTOR,'#main-content > div > form > p > button').click()
-# elem = wd.find_element_by_css_selector('#session_key')
-elem = wd.find_element(by=By.CSS_SELECTOR, value= '#session_key')
-elem.send_keys('username')
-# elem = wd.find_element_by_css_selector('#session_password')
-elem = wd.find_element(By.CSS_SELECTOR,value = '#session_password')
-elem.send_keys('password')
-wd.find_element(By.CSS_SELECTOR, value='#main-content > div > div > div > form > button').click()
-
-# Get to the job page
-wd.get(url_bp)
-wd.find_element(By.CSS_SELECTOR, value='#main > div.org-grid__content-height-enforcer > div > section > div > div > a').click()
-
-for handle in wd.window_handles:
-    wd.switch_to.window(handle)
-    if 'Search all Jobs' in wd.title:
-        break
-Job_list_window = wd.current_window_handle
-
-# scroll to the bottom
+def main():
+    # random_number = random.randint(2,8)
+    driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
+    company_name = 'eni'
+    instance = linkedin(driver,company_name,5)
+    instance.login()
+    data = instance.data_scraping()
+    data.to_excel(company_name + 'data.xlsx')
 
 
-def get_page_info_v2(wd,url):
-    wd.get(url)
-    # list_items = wd.find_elements_by_class_name("occludable-update")
-    list_items = wd.find_elements(By.CSS_SELECTOR, value = '.occludable-update')
-    # job_info = wd.find_elements(By.CLASS_NAME,value = 'jobs-search-results__list-item')
-    # block = wd.find_element(By.CSS_SELECTOR,'body > div.application-outlet > div.authentication-outlet > div.job-search-ext > div.jobs-search-two-pane__wrapper > div > section.jobs-search__left-rail > div > div > ul')
-    # code = block.get_attribute('innerHTML')
-    # job_id = re.findall('<div data-job-id="(\d*)"',code)
+class linkedin:
 
-    position = []
-    company =[]
-    location = []
-    details = []    
+    def __init__(self,driver,company_name,page_number) -> None:
 
-    #if it's the last page, the last item of list_items cannot be used.
-    if len(list_items) < 25:
-        list_items.pop()
-    else:
-        pass
+        '''
+        driver: the selenium driver to use,
+        company_name: the name of the compnay in linkedIn company page url, for example, bp in 'https://www.linkedin.com/company/bp/jobs/'
+        page_number: the number of pages you'd like to get in the search results, if full data, check the how many pages is shown in linkedIN
+        '''
 
-    for job in list_items:
-    # executes JavaScript to scroll the div into view
-        wd.execute_script("arguments[0].scrollIntoView();", job)
-        job.click()
-        time.sleep(3)
-        # get info:
-        # [position, company, location] = job.text.split('\n')[:3]
-        position_text = job.text.split('\n')[0]
-        company_text = job.text.split('\n')[1]
-        location_text = job.text.split('\n')[2]
+        self.driver = driver
+        self.company_job_url = r'https://www.linkedin.com/company/bp/jobs/'
+        self.page_number = page_number
+        self.company_name = company_name
+        self.search_results_initial_url = r'https://www.linkedin.com/jobs/search/?geoId=92000000&f_C=1389%2C1391&keywords=&origin=COMPANY_PAGE_JOBS_KEYWORD'
+        self.search_results_first_page = r'https://www.linkedin.com/jobs/search/?f_C=1389%2C1391&geoId=92000000'
+        self.company_search_url_list = [self.search_results_first_page]
+    
+    def login(self):
+        # login with username and password
+        try:
+            self.driver.get(self.company_job_url)
+            time.sleep(5)
+            self.driver.find_element(By.CSS_SELECTOR,'#main-content > div > form > p > button').click()
+            # elem = wd.find_element_by_css_selector('#session_key')
+            elem = self.driver.find_element(by=By.CSS_SELECTOR, value= '#session_key')
+            elem.send_keys('username')
+            # elem = wd.find_element_by_css_selector('#session_password')
+            elem = self.driver.find_element(By.CSS_SELECTOR,value = '#session_password')
+            elem.send_keys('password')
+            self.driver.find_element(By.CSS_SELECTOR, value='#main-content > div > div > div > form > button').click()
+        except:
+            # in case it's already signed in
+            pass
+            
+        # Get to the job page
+        self.driver.get(r'https://www.linkedin.com/company/' + self.company_name+'/jobs/')
+        self.driver.find_element(By.CSS_SELECTOR, value='#main > div.org-grid__content-height-enforcer > div > section > div > div > a').click()
 
-        if len(position_text) == 0:
-            position.append('N/A')
-        else:
-            position.append(position_text)
+        # switch to the newly opened window
 
-        if len(company_text) == 0:
-            company.append('N/A')
-        else:
-            company.append(company_text)            
-        
-        if len(location_text) == 0:
-            location.append('N/A')
-        else:
-            location.append(location_text)
-        # position.append(job.text.split('\n')[0])
-        # company.append(job.text.split('\n')[1])
-        # location.append(job.text.split('\n')[2])
-        detail = wd.find_element_by_id("job-details").text
-        if len(detail) == 0:
-            details.append('N/A')
-        else:
-            details.append(detail)
-
-    # temp = pd.DataFrame({'Job id':job_id,'Job title':position, 'Job location':location, 'Company': company, 'Details':details}) 
-    temp = pd.DataFrame({'Job title':position, 'Job location':location, 'Company': company, 'Details':details}) 
-    return temp
-
-
-
-def get_page_info(wd,url):
-    wd.get(url)
-    ac = ActionChains(wd)
-    ac.move_to_element(wd.find_element(By.ID,'ember126')).perform()
-    random_number = 5
-    # job_info = wd.find_elements_by_class_name('jobs-search-results__list-item')
-    job_info = wd.find_elements(By.CLASS_NAME,value = 'jobs-search-results__list-item')
-    # ac.move_to_element(wd.find_element(By.ID,'compactfooter-copyright')).perform()
-    time.sleep(10)
-    job_title=[]
-    location = []
-    company = []
-    for title in job_info:
-        job_title.append(title.text.split('\n')[0])
-        company.append(title.text.split('\n')[1])
-        location.append(title.text.split('\n')[2])
-        wd.implicitly_wait(random_number)
-        time.sleep(random_number)
-
-        left_rail = wd.find_element(By.CSS_SELECTOR,'.jobs-search__left-rail')
-        wd.implicitly_wait(2)
-        time.sleep(3)
-        left_rail.send_keys(Keys.PAGE_DOWN)
-
-    block = wd.find_element(By.CSS_SELECTOR,'body > div.application-outlet > div.authentication-outlet > div.job-search-ext > div.jobs-search-two-pane__wrapper > div > section.jobs-search__left-rail > div > div > ul')
-    code = block.get_attribute('innerHTML')
-
-    job_id = re.findall('<div data-job-id="(\d*)"',code)
-    # job_title=re.findall('disabled ember-view job-card-container__link job-card-list__title">\n              (.*?)\n            </a>',code)
-    # location = re.findall('job-card-container__metadata-item">(.*?)</li>',code)
-
-    temp = pd.DataFrame({'Job id':job_id,'Job title':job_title, 'Job location':location, 'Company': company}) 
-
-    return temp  
-# ac.move_to_element(wd.find_element(By.ID,'compactfooter-copyright')).perform()
-
-def bp_url_generator(page):
-    url_bp_list=['https://www.linkedin.com/jobs/search/?f_C=1389%2C1391&geoId=92000000']
-    bp_url_base = r'https://www.linkedin.com/jobs/search/?f_C=1389%2C1391&geoId=92000000&start='
-
-    for i in range(1,page):
-        url =bp_url_base+ str(i*25)
-        url_bp_list.append(url)
-    return url_bp_list
-
-
-company_base_urls ={
-    'bp':'https://www.linkedin.com/jobs/search/?f_C=1389%2C1391&geoId=92000000',
-    'Shell':'https://www.linkedin.com/jobs/search/?f_C=1271%2C28446844%2C41804%2C28452536&geoId=92000000'
-}
-
-def company_jobs_url_generator(base_url,page_number):
-    url_list = [base_url]
-    url_from_page_2 = base_url + '&start='
-    for i in range(1,page_number):
-        url = url_from_page_2 + str(i*25)
-        url_list.append(url)
-    return url_list
-        
-
-
-table = pd.DataFrame()
-
-shell_url_list = company_jobs_url_generator(company_base_urls['Shell'],40)
-
-for url in shell_url_list[0:5]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('Shell data to page 5.xlsx')
-
-for url in shell_url_list[5:10]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('Shell data to page 10.xlsx')
-
-
-for url in shell_url_list[10:15]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('Shell data to page 15.xlsx')
-
-for url in shell_url_list[15:20]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('Shell data to page 20.xlsx')
-
-for url in shell_url_list[20:25]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('Shell data to page 25.xlsx')
-
-# For the data scraping with a few pages for each try
-url_list = bp_url_generator(27)
-for url in url_list[0:5]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('bp data to page 5.xlsx')
-
-
-for url in url_list[5:10]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
-
-table.to_excel('bp data to page 10.xlsx')
+        for handle in self.driver.window_handles:
+            self.driver.switch_to.window(handle)
+            if 'Search all Jobs' in self.driver.title:
+                break
+        # Job_list_window = self.driver.current_window_handle
+        self.search_results_initial_url = self.driver.current_url
+        self.base_urls_generator()    
     
 
-for url in url_list[10:15]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
+    def base_urls_generator(self):
+        self.company_job_url = r'https://www.linkedin.com/company/' + self.company_name+'/jobs/'
+        filters = self.search_results_initial_url.split('/')[5]
+        str1 = filters.split('&')[1]
+        str2 = filters.split('&')[0][1:]
+        self.search_results_first_page = r'https://www.linkedin.com/jobs/search/?' + str1 +'&' + str2
 
-table.to_excel('bp data to page 15.xlsx')
 
-for url in url_list[15:20]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
+    def page_url_generator(self):
+        self.company_search_url_list = [self.search_results_first_page]
+        for i in range(1,self.page_number):
+            url =self.company_search_url_list[0] + '&start=' + str(i*25)
+            self.company_search_url_list.append(url)
+        
+        #     # url_list = [base_url]
+        # url_from_page_2 = base_url + '&start='
+        # for i in range(1,page_number):
+        #     url = url_from_page_2 + str(i*25)
+        #     url_list.append(url)
+        # return url_list
 
-table.to_excel('bp data to page 20.xlsx')
+    def get_page_info(self,url):
+        '''
+        For every page of the job list, get all the infomation and return it as a dataframe
+        '''
+        try:
+            self.driver.get(url)
+        # list_items = wd.find_elements_by_class_name("occludable-update")
+            list_items = self.driver.find_elements(By.CSS_SELECTOR, value = '.occludable-update')
+        except:
+            time.sleep(60+random.randint(2,8))
+            self.driver.get(url)
+            list_items = self.driver.find_elements(By.CSS_SELECTOR, value = '.occludable-update')
+        # job_info = wd.find_elements(By.CLASS_NAME,value = 'jobs-search-results__list-item')
+        # block = wd.find_element(By.CSS_SELECTOR,'body > div.application-outlet > div.authentication-outlet > div.job-search-ext > div.jobs-search-two-pane__wrapper > div > section.jobs-search__left-rail > div > div > ul')
+        # code = block.get_attribute('innerHTML')
+        # job_id = re.findall('<div data-job-id="(\d*)"',code)
 
-for url in url_list[20:]:
-    temp = get_page_info_v2(wd,url)
-    table = pd.concat([table,temp])
+        position = []
+        company =[]
+        location = []
+        details = []    
 
-table.to_excel('bp data full.xlsx')
+        #if it's the last page, the last item of list_items cannot be used.
+        if len(list_items) < 25:
+            list_items.pop()
+        else:
+            pass
+
+        for job in list_items:
+        # executes JavaScript to scroll the div into view
+            self.driver.execute_script("arguments[0].scrollIntoView();", job)
+            job.click()
+            time.sleep(3)
+            # get info:
+            # [position, company, location] = job.text.split('\n')[:3]
+            position_text = job.text.split('\n')[0]
+            company_text = job.text.split('\n')[1]
+            location_text = job.text.split('\n')[2]
+
+            if len(position_text) == 0:
+                position.append('N/A')
+            else:
+                position.append(position_text)
+
+            if len(company_text) == 0:
+                company.append('N/A')
+            else:
+                company.append(company_text)            
+            
+            if len(location_text) == 0:
+                location.append('N/A')
+            else:
+                location.append(location_text)
+            # position.append(job.text.split('\n')[0])
+            # company.append(job.text.split('\n')[1])
+            # location.append(job.text.split('\n')[2])
+            detail = self.driver.find_element_by_id("job-details").text
+            if len(detail) == 0:
+                details.append('N/A')
+            else:
+                details.append(detail)
+
+        # temp = pd.DataFrame({'Job id':job_id,'Job title':position, 'Job location':location, 'Company': company, 'Details':details}) 
+        job_data = pd.DataFrame({'Job title':position, 'Job location':location, 'Company': company, 'Details':details}) 
+        return job_data
+
+    def data_scraping(self):
+        data = pd.DataFrame()
+        self.page_url_generator()
+        for page in self.company_search_url_list:
+            page_data = self.get_page_info(page)
+            data = pd.concat([data, page_data])
+        return data
 
 
 
@@ -331,12 +270,8 @@ class shell:
             job_entry = self.extract_job_info_in_description_page_v2(url)
             Job_data = pd.concat([Job_data,job_entry])
         return Job_data
-    
-    
 
-# url_search_results = r'https://krb-sjobs.brassring.com/tgnewui/search/home/home?partnerid=30080&siteid=6558#keyWordSearch='
 
-driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
 
 class total:
     
@@ -357,10 +292,10 @@ class total:
 
         # scroll to the bottom
         try:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         except:
             time.sleep(10)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         # click all the 'show more jobs' to get the full list of jobs
         try:
@@ -371,7 +306,7 @@ class total:
             pass
 
         # get the links for each job
-        items = driver.find_elements(By.CSS_SELECTOR,'[class = "jobProperty jobtitle"]')
+        items = self.driver.find_elements(By.CSS_SELECTOR,'[class = "jobProperty jobtitle"]')
         links = [item.get_attribute('href') for item in items]
     
         return links
